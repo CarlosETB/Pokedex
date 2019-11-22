c60368e8383453a382dd6f205013425bb8a305d8
FahRamatis <fah.ramatis@gmail.com>
2019-11-22 11:31:30 -0300

    fix effect on home screen

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -14,8 +14,8 @@ import Search from "../../components/Search";
 export default function HomeScreen({ navigation }) {
   const [numberPokemons, setNumberPokemons] = useState(4);
   const [pokemons, setPokemons] = useState([]);
-  const [search, setSearch] = useState('')
-  const [searchErrors, setSearchErrors] = useState(false)
+  const [search, setSearch] = useState("");
+  const [searchErrors, setSearchErrors] = useState(false);
   const { loading, error, data } = useQuery(GET_POKEMONS, {
     variables: {
       pokemons_number: numberPokemons
@@ -26,34 +26,37 @@ export default function HomeScreen({ navigation }) {
     variables: {
       pokemon_name: search
     }
-  })
+  });
 
-  useEffect(() => {
-    data && setPokemons(data.pokemons)
-  }, [])
+  useEffect(() => data && setPokemons(data.pokemons))
 
   function searchPokemons() {
-    if (search.length === 0) return setPokemons(data.pokemons)
-    if (pokeSearch.error) return setSearchErrors(true)
-    if (pokeSearch.data) return setPokemons([pokeSearch.data.pokemon])
+    if (search.length === 0) return setPokemons(data.pokemons);
+    if (pokeSearch.error) return setSearchErrors(true);
+    if (pokeSearch.data) return setPokemons([pokeSearch.data.pokemon]);
   }
 
   return (
     <Container>
-      <Search value={search} onChangeText={text => setSearch(text)} onPress={() => searchPokemons()} />
+      <Search
+        value={search}
+        onChangeText={text => setSearch(text)}
+        onPress={() => searchPokemons()}
+      />
       <ScrollView showsVerticalScrollIndicator={false}>
         {loading && <Text>Carregando...</Text>}
         {error && <Text>Error</Text>}
         {searchErrors && <Text>Sua pesquisa esta muito erradinha</Text>}
-        {pokemons && pokemons.map(poke => (
-          <Box
-            key={poke.id}
-            type={poke.types[0]}
-            number={poke.number}
-            source={{ uri: poke.image }}
-            name={poke.name}
-          />
-        ))}
+        {pokemons &&
+          pokemons.map(poke => (
+            <Box
+              key={poke.id}
+              type={poke.types[0]}
+              number={poke.number}
+              source={{ uri: poke.image }}
+              name={poke.name}
+            />
+          ))}
         <Button onPress={() => setNumberPokemons(numberPokemons + 4)}>
           <Text>Carregar mais...</Text>
         </Button>
